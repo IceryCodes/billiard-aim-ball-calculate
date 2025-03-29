0c838c4e04118f940b5ff568d8ebaad47fd29698
Icery <Icery@Icery.tw>
2025-03-30 05:54:10 +0800

    feat(add-cushion-calculation): remove duplicate GA.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Analytics } from '@vercel/analytics/next';
-import Script from 'next/script';
 
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import { ToastProvider } from '@/contexts/ToastContext';
@@ -20,21 +19,7 @@ const queryClient = new QueryClient({
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="tc" className="scroll-smooth" suppressHydrationWarning>
-      <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
-          `}
-        </Script>
-      </head>
+    <html lang="zh-TW" className="scroll-smooth" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <QueryClientProvider client={queryClient}>
